400355ce3ab637be71bbfa6d39291ffcf856fbe0
WimVdSAllPhi <57750279+WimVdSAllPhi@users.noreply.github.com>
2022-02-22 15:05:04 +0100

    Added Local Storage + init Card

--- BlazorEcommerce/BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Program.cs
@@ -4,12 +4,16 @@ global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Shared;
 
 using BlazorEcommerce.Client;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// LocalStorage
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
